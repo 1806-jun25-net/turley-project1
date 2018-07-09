d76b239668af6488d5cbf9c4864c5150bf7f0a44
Zach <zjturley@gmail.com>
2018-07-09 16:20:51 -0400

    Added json and db setup to main program

--- PizzaPlanet/PizzaPlanet.Console/Program.cs
+++ PizzaPlanet/PizzaPlanet.Console/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using PizzaPlanet.DBData;
 
 namespace PizzaPlanet.Application
 {
@@ -6,6 +10,15 @@ namespace PizzaPlanet.Application
     {
         static void Main(string[] args)
         {
+            var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            IConfigurationRoot configuration = configBuilder.Build();
+            var optionsBuilder = new DbContextOptionsBuilder<Project1PizzaPlanetContext>();
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("PizzaPlanet"));
+            var options = optionsBuilder.Options;
+            //new YourAppDbContext(options)
+            
             //start console app
             Console.StartScreen();
         }
--- PizzaPlanet/PizzaPlanetDBConnector/Project1PizzaPlanetContext.cs
+++ PizzaPlanet/PizzaPlanetDBConnector/Project1PizzaPlanetContext.cs
@@ -24,8 +24,7 @@ namespace PizzaPlanet.DBData
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
-                optionsBuilder.UseSqlServer("Server=tcp:turley-1806.database.windows.net,1433;Initial Catalog=Project1-PizzaPlanet;Persist Security Info=False;User ID=turley;Password=Password123;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                //JSON
             }
         }
 
