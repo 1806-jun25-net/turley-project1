4ce8217cc96eaa8332f3a37c35eac6f279fd7d7a
Zach <zjturley@gmail.com>
2018-07-11 08:24:04 -0400

    updated unit tests

--- PizzaPlanet/PizzaPlanet.Testing/OrderTest.cs
+++ PizzaPlanet/PizzaPlanet.Testing/OrderTest.cs
@@ -10,32 +10,35 @@ namespace PizzaPlanet.Testing
         private static int MaxPizza = (int)Math.Pow(2, 24) - 1 - 4 - 1;
         private static Location TestLoc;
         private static int StoreNum = 100;
+        private static User user;
         private static void SetUp()
         {
             TestLoc = new Location(StoreNum);
+            user = new User("test");
+            TestLoc.FullInventory();
         }
-
-        //idfull, addpizza, remove pizza, canaddpizza
-        [Fact]
-        public void OrderIdFullTest()
-        {
-            SetUp();
-            Order o = new Order(null,TestLoc);
-            o.AddPizza(new Pizza(LargeHandCheese));
-            TestLoc.PlaceOrder(o);
-            Assert.Equal(1, o.Id);
-            Assert.Equal(1.100M, o.IdFull());
-        }
+    
+        // requires repo
+        //[Fact]
+        //public void OrderIdFullTest()
+        //{
+        //    SetUp();
+        //    Order o = new Order(user,TestLoc,DateTime.Now,0);
+        //    o.AddPizza(new Pizza(LargeHandCheese));
+        //    TestLoc.PlaceOrder(o);
+        //    Assert.Equal(1, o.Id);
+        //    Assert.Equal(1.100M, o.IdFull());
+        //}
 
         [Fact]
         public void AddRemoveNumPizzaTest()
         {
             SetUp();
-            Order o = new Order(null, TestLoc);
+            Order o = new Order(user, TestLoc, DateTime.Now, 0);
             Assert.Equal(0, o.NumPizza);
             o.AddPizza(new Pizza(LargeHandCheese));
             Assert.Equal(1,o.NumPizza);
-            o.RemovePizza(LargeHandCheese);
+            o.RemovePizza(0);
             Assert.Equal(0, o.NumPizza);
         }
 
@@ -43,7 +46,7 @@ namespace PizzaPlanet.Testing
         public void CanAddNumPizzaTest()
         {
             SetUp();
-            Order o = new Order(null, TestLoc);
+            Order o = new Order(user, TestLoc, DateTime.Now, 0);
             Assert.True(o.CanAddPizza());
             for(int i =0;i<Order.MaxPizzas;i++)
                 o.AddPizza(new Pizza(LargeHandCheese));
@@ -55,7 +58,7 @@ namespace PizzaPlanet.Testing
         public void CanAddPricePizzaTest()
         {
             SetUp();
-            Order o = new Order(null, TestLoc);
+            Order o = new Order(user, TestLoc, DateTime.Now, 0);
             Assert.True(o.CanAddPizza());
             Pizza p = new Pizza(MaxPizza);
             for (int i = 0; i < 500/p.Price(); i++)
