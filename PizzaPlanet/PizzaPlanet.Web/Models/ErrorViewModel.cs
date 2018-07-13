0e0e8f6d0abe57294abfdda4bea59a0151cd9da0
Zach <zjturley@gmail.com>
2018-07-13 11:17:02 -0400

    MVC Template

---
+++ PizzaPlanet/PizzaPlanet.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace PizzaPlanet.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
